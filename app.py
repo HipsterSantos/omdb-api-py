727824930cb5b93778025ec3fe504df517f3d403
HipsterSantos <santoscampos269@gmail.com>
2024-04-13 23:34:38 -0400

    handling network error: Adding support to handling network error

--- app.py
+++ app.py
@@ -1,12 +1,19 @@
-import requests
-import collections
-api_key = "915ad862"
-namedTuttple = collections.namedtuple('movies',"Title,imdbID,Year,Poster,Type")
-url = f"http://www.omdbapi.com/?apikey={api_key}&s=Dracula&plot=full"
-r = requests.get(url)
-result = r.json()["Search"]
-v = []
-for c in result:
-    cc = namedTuttple(**c)
-    v.append(cc)
-print(v[0])
+from movie_search import MovieList
+
+def main():
+    event_loop()
+def event_loop():
+    searchTitle = "ONCE_THROUGH_LOOP"
+    while searchTitle != 'x':
+        try:
+            searchTitle =   input('enter the text to be searched')
+            if searchTitle != 'x':
+                movies = MovieList(searchTitle)
+                result = movies.perform_search()
+                for c in result:
+                    print("{} -- {}".format(c.Year,c.Title))
+        except:
+            print("invalid input, goddamn it!")
+if __name__ == "__main__":
+    main()
+
--- movie_search.py
+++ movie_search.py
@@ -6,9 +6,13 @@ api_key = "915ad862"
 
 class MovieList:
     def __init__(self,search: str):
+        if not search or not search.strip():
+            raise ValueError('Please provide a proper search value')
         self.search = search
     def perform_search(self):
         url = f"http://www.omdbapi.com/?apikey={api_key}&s={self.search}&plot=full"
         response = requests.get(url)
         data = response.json()
-        return [Movies(**d) for d in data]
+        movies = [Movies(**d) for d in data["Search"]]
+        movies.sort(key=lambda movie: movie.Year, reverse=True)
+        return movies
